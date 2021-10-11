567e22ec1b07954060c1615d5959bd3228793a93
Olawalecodes <iwaloyeo@gmail.com>
2021-10-11 23:30:38 +0100

    updates completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const hambuger = document.getElementById("hamburger");
 const navUL = document.getElementById("nav-ul");
+const navContainer = document.getElementById("fixed");
 
 hamburger.addEventListener('click', () => {
   navUL.classList.toggle("show");
-})
+  navContainer.classList.toggle("fixed");
+});
